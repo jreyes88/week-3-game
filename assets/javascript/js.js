521892897ea54b971f47de286d8785576598e1f2
Joey Reyes <jreyes88@gmail.com>
2016-05-08 19:17:38 -0500

    hangman version 1

---
+++ assets/javascript/js.js
@@ -0,0 +1,62 @@
+var computerGuess = "";
+var concealedWord = "";
+var guessesRemaining = 15;
+var guess;
+
+var hangman = {
+
+	words: ["all american slam", "the grand slamwich", "belgian waffle slam", "lumberjack slam", "french toast slam", "fit slam", "grand slam slugger", "red white and blue slam", "four dollar everyday value slam", "honey jalapeno bacon slam"],
+	
+	wordSelect: function() {
+		var randomNumber = [Math.floor(Math.random() * 10)];
+		computerGuess = this.words[randomNumber];
+		console.log(computerGuess);
+	},
+
+	clueWord: function() {
+		for (i = 0; i < computerGuess.length; i++) {
+			concealedWord += "<p class='underScore'>_</p>";
+			document.querySelector("#currentWord").innerHTML = concealedWord;
+			//console.log(concealedWord);
+		}
+	},
+
+	wordGuessing: function() {
+		if (computerGuess.indexOf(guess) !== -1) {
+			console.log(computerGuess + guess);
+			console.log("Contains This Letter");
+		} else {
+			guessesRemaining -= 1;
+			document.querySelector("#stats").innerHTML = "<p>Guesses Remaining: </p>" + guessesRemaining;
+			console.log("Does Not Contain This Letter " + guessesRemaining);
+		}
+	},
+
+	updateWord: function() {
+		for (var i = 0; i < computerGuess.length; i++) {
+			if (guess === computerGuess[i]) {
+				console.log(computerGuess[i] + i + "okay");
+				var par = document.getElementsByClassName('underScore');
+				par[i].innerHTML = guess;
+				console.log(par[i]);
+			}
+		}
+	}
+
+}
+
+hangman.wordSelect();
+hangman.clueWord();
+
+document.onkeyup = function(event) {
+	guess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(guess);
+	hangman.wordGuessing();
+	hangman.updateWord();
+}
+
+
+
+
+	// var par = document.getElementsByClassName('underScore');
+	// console.log(par[i]);
