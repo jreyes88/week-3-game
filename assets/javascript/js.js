1152da6987ea39c2bb6b0e0739d2368df4be6e6d
Joey Reyes <jreyes88@gmail.com>
2016-05-10 19:33:00 -0500

    getting there

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -3,14 +3,14 @@ var concealedWord = "";
 var guessesRemaining = 15;
 var guess;
 var wins = 0;
-var victory = 0;
+var victory = "";
 var lettersGuessed = "";
 
 var hangman = {
 
 	words: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"],
 
-	updateHTML: function() {
+	initialHTML: function() {
 		var html = "<p>Wins: </p>" + wins + "<p>Guesses Remaining: </p>" + guessesRemaining + "<p>Letters Guessed: </p>" + lettersGuessed;
 		document.querySelector("#stats").innerHTML = html;
 	},
@@ -18,12 +18,12 @@ var hangman = {
 	wordSelect: function() {
 		var randomNumber = [Math.floor(Math.random() * 10)];
 		computerGuess = this.words[randomNumber];
-		//console.log(computerGuess);
+		console.log(computerGuess);
 	},
 
 	clueWord: function() {
 		for (i = 0; i < computerGuess.length; i++) {
-			concealedWord += "<p class='underScore'>_</p>";
+			concealedWord += "<p class='underScore'>_ </p>";
 			document.querySelector("#currentWord").innerHTML = concealedWord;
 			//console.log(concealedWord);
 		}
@@ -37,20 +37,6 @@ var hangman = {
 			guessesRemaining -= 1;
 			lettersGuessed += guess;
 			//console.log("Does Not Contain This Letter " + guessesRemaining);
-			if (guessesRemaining === 0) {
-				alert("You Lose!");
-				concealedWord = "";
-				var randomNumber = [Math.floor(Math.random() * 10)];
-				computerGuess = this.words[randomNumber];
-				for (i = 0; i < computerGuess.length; i++) {
-					concealedWord += "<p class='underScore'>_</p>";
-					document.querySelector("#currentWord").innerHTML = concealedWord;
-					//console.log(concealedWord);
-				};
-				guessesRemaining = 15;
-				lettersGuessed = "";
-				this.updateHTML();
-			}
 		}
 	},
 
@@ -60,48 +46,63 @@ var hangman = {
 				//console.log(computerGuess[i] + i + "okay");
 				var par = document.getElementsByClassName('underScore');
 				par[i].innerHTML = guess;
-				//console.log(par[i]);
-				victory++;
-				console.log(victory);
-				console.log(computerGuess.length);
+				//console.log(computerGuess.length);
 				//console.log(wins);
+				//console.log(concealedWord);
+            	victory = victory + guess;
+        		console.log(victory);
+        		for (var j = 0; j < victory.length; j++) {
+        			if (victory[i] === guess) {
+        				victory !== victory + guess};
+        		};
 			}
 		}
 	},
+
+	updateHTML: function() {
+		guessesRemaining: 15;
+		var html = "<p>Wins: </p>" + wins + "<p>Guesses Remaining: </p>" + guessesRemaining + "<p>Letters Guessed: </p>" + lettersGuessed;
+		document.querySelector("#stats").innerHTML = html;
+	},
+
+	resetHTML: function() {
+		lettersGuessed = " ";
+		guessesRemaining = 15;
+		computerGuess = [];
+		concealedWord = [];
+		this.wordSelect();
+		this.clueWord();
+		victory = [];
+	},
+
+	bigLose: function() {
+		if (guessesRemaining === 0) {
+			alert("You Lose!");
+			this.resetHTML();	
+		}
+	},
+
 	bigWin: function() {
-		if (victory === computerGuess.length) {
+		if (victory.length === computerGuess.length) {
 			alert("You Win!");
-			concealedWord = "";
-			var randomNumber = [Math.floor(Math.random() * 10)];
-			computerGuess = this.words[randomNumber];
-			for (i = 0; i < computerGuess.length; i++) {
-				concealedWord += "<p class='underScore'>_</p>";
-				document.querySelector("#currentWord").innerHTML = concealedWord;
-				//console.log(concealedWord);
-			};
-			wins+=1;
-			guessesRemaining = 15;
-			lettersGuessed = "";
-			this.updateHTML();
+			wins += 1;
+			this.resetHTML();
 		}
 	}
 }
 
+hangman.initialHTML();
 hangman.wordSelect();
 hangman.clueWord();
-hangman.updateHTML();
 
 document.onkeyup = function(event) {
 	guess = String.fromCharCode(event.keyCode).toLowerCase();
 	//console.log(guess);
 	hangman.wordGuessing();
 	hangman.updateWord();
-	hangman.updateHTML();
+	hangman.bigLose();
 	hangman.bigWin();
+	hangman.updateHTML();
 }
-
-
-
-
 	// var par = document.getElementsByClassName('underScore');
 	// console.log(par[i]);
